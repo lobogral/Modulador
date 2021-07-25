910a3c76ba805b61e1b26317cb352753f3be3a04
Camilo Sabogal <lobogral@gmail.com>
2021-07-25 17:04:09 -0500

    Agrego opción de importar en árbol

--- src/preparador.py
+++ src/preparador.py
@@ -2,14 +2,35 @@ from sys import path, argv
 import re
 import os
 
+# Comando clonar
+comandoClonar = 'git clone https://github.com/lobogral/'
+
+# Importa los módulos que necesita el programa
+def importarModulos():
+    if os.path.isfile('dependencias.txt'): 
+        arch = open('dependencias.txt', 'r')
+        modulos = [linea.rstrip('\n') for linea in arch.readlines()]
+        arch.close()
+
+        if (not os.path.isdir('módulos')): 
+            os.mkdir('módulos')
+
+        os.chdir('módulos')
+        for modulo in modulos:
+            if (not os.path.isdir(modulo)):
+                os.system(comandoClonar + modulo)
+                os.chdir(modulo)
+                importarModulos()
+                os.chdir('..')
+        os.chdir('..')
+
+
 # Agrego en un archivo el programa correspondiente
 arch = open("programa.txt", "w")
 arch.write(argv[1])
 arch.close()
 
 # Importa el programa correspondiente
-comandoClonar = 'git clone https://github.com/lobogral/'
-
 os.chdir('..')
 if (not os.path.isdir('codes')):
     os.mkdir('codes')
@@ -18,17 +39,5 @@ if (not os.path.isdir(argv[1])):
     os.system(comandoClonar + argv[1])
 
 os.chdir(argv[1])
+importarModulos()
 
-# Importa los módulos que necesita el programa
-arch = open('dependencias.txt', 'r')
-lineasDep = arch.readlines()
-arch.close()
-modulos = [linea.rstrip('\n') for linea in lineasDep]
-
-if (not os.path.isdir('módulos')): 
-    os.mkdir('módulos')
-os.chdir('módulos')
-
-for modulo in modulos:
-    if (not os.path.isdir(modulo)):
-        os.system(comandoClonar + modulo)
