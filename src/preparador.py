b07a98d7446a22aa97c32c57246c82da6821a4a2
Camilo Sabogal <lobogral@gmail.com>
2021-07-25 17:37:23 -0500

    Mejoro comprensión del preparador

--- src/preparador.py
+++ src/preparador.py
@@ -7,23 +7,18 @@ comandoClonar = 'git clone https://github.com/lobogral/'
 
 # Importa los módulos que necesita el programa
 def importarModulos():
-    if os.path.isfile('dependencias.txt'): 
-        arch = open('dependencias.txt', 'r')
-        modulos = [linea.rstrip('\n') for linea in arch.readlines()]
-        arch.close()
+    arch = open('dependencias.txt', 'r')
+    modulos = [linea.rstrip('\n') for linea in arch.readlines()]
+    arch.close()
 
-        if (not os.path.isdir('módulos')): 
-            os.mkdir('módulos')
-
-        os.chdir('módulos')
-        for modulo in modulos:
-            if (not os.path.isdir(modulo)):
-                os.system(comandoClonar + modulo)
-                os.chdir(modulo)
-                importarModulos()
-                os.chdir('..')
+    os.mkdir('módulos')
+    os.chdir('módulos')
+    for modulo in modulos:
+        os.system(comandoClonar + modulo)
+        os.chdir(modulo)
+        if os.path.isfile('dependencias.txt'): importarModulos()
         os.chdir('..')
-
+    os.chdir('..')
 
 # Agrego en un archivo el programa correspondiente
 arch = open("programa.txt", "w")
@@ -32,12 +27,12 @@ arch.close()
 
 # Importa el programa correspondiente
 os.chdir('..')
-if (not os.path.isdir('codes')):
-    os.mkdir('codes')
+if (not os.path.isdir('codes')): os.mkdir('codes')
 os.chdir('codes')
+
 if (not os.path.isdir(argv[1])):
     os.system(comandoClonar + argv[1])
-
-os.chdir(argv[1])
-importarModulos()
+    os.chdir(argv[1])
+    if os.path.isfile('dependencias.txt'): importarModulos()
+    os.chdir('..')
 
