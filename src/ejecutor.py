5ab6f55e3260782905110509a9afa0c8d8da11f3
Camilo Sabogal <lobogral@gmail.com>
2021-07-26 13:12:24 -0500

    Agrego estructura de árbol a ejecutor.py

--- src/ejecutor.py
+++ src/ejecutor.py
@@ -1,20 +1,22 @@
 from sys import path, argv
 import re
+import os
+
+# Agrego los módulos correspondientes a un programa
+def agregarModulos(ruta): 
+    for modulo in os.listdir(ruta):
+        path.append(ruta + modulo + '/código/')
+        if os.path.isdir(ruta + modulo + '/módulos/'):
+            agregarModulos(ruta + modulo + '/módulos/')
 
 # Busco el programa a ejecutar
 arch = open("programa.txt", "r")
 rutaPrograma = '../codes/' + arch.readline() + '/'
 arch.close()
 
-# Busco las dependencias a agregar
-arch = open(rutaPrograma + 'dependencias.txt', 'r')
-lineasDep = arch.readlines()
-arch.close()
-modulos = [linea.rstrip('\n') for linea in lineasDep]
-
-# Agrego las dependencias
-for modulo in modulos:
-    path.append(rutaPrograma + '/módulos/' + modulo + '/código/')
+# Agrego los módulos al programa
+if os.path.isdir(rutaPrograma + '/módulos/'):
+    agregarModulos(rutaPrograma + '/módulos/')
 
 # Ejecuto el programa
 rutaSubPrograma = rutaPrograma + '/código/' + argv[1]
