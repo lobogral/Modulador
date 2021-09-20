514630f3caffb3ffa949b5da9543720d5fcddbac
Camilo Sabogal <lobogral@gmail.com>
2021-09-19 22:59:04 -0500

    Cambio estructura de .bat a .py

--- src/modulador.py
+++ src/modulador.py
@@ -3,11 +3,19 @@ from importador import importarRepositorio
 from ejecutor import ejecutarModulo
 from agregador import agregarRepositorio
 
+import os
+
 opcion = argv[1]
 
 if opcion == '-i':
     urlRep = argv[2]
     importarRepositorio(urlRep)
+
+    if not os.path.isdir('entVir'):
+        os.system("py -m venv entVir")
+    os.system("call entVir\Scripts/activate.bat & pip list & py Grafica.py & deactivate")
+    os.system("pip list")
+    
 elif opcion == '-e':
     nombreModRep = argv[2]
     argv = argv[2:]
