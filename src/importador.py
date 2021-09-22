62b643633cb1aeb7688a9978674be8e8197ac0cf
Camilo Sabogal <lobogral@gmail.com>
2021-09-21 23:14:55 -0500

    agrego autodocumentacion al codigo

--- src/importador.py
+++ src/importador.py
@@ -4,7 +4,7 @@ Realiza importaciones de modulos para desarrollo
 from sys import path
 import os
 
-def __importarDependencias(urlsDepsImportadas, nombreRep):
+def __importarDepsGit(urlsDepsImportadas, nombreRep):
     arch = open(nombreRep + '/depsGit.txt', 'r')
     urlsDepsImportar = [lineaArch.rstrip('\n') for lineaArch in arch.readlines()]
     urlsDepsImportar = [val for val in urlsDepsImportar if val not in urlsDepsImportadas]
@@ -12,13 +12,31 @@ def __importarDependencias(urlsDepsImportadas, nombreRep):
     
     for urlDepImportar in urlsDepsImportar:
         if urlDepImportar not in urlsDepsImportadas:
-            urlsDepsImportadas += [urlDepImportar]
             os.system('git clone ' + urlDepImportar)
-            nombreDep = urlDepImportar.split('/').pop()
-            if os.path.isfile(nombreDep + '/depsGit.txt'):
-                urlsDepsImportadas = __importarDependencias(urlsDepsImportadas, nombreDep)
+            nombreRep = urlDepImportar.split('/').pop()
+            urlsDepsImportadas += [urlDepImportar]
+            if os.path.isfile(nombreRep + '/depsGit.txt'):
+                urlsDepsImportadas = __importarDepsGit(urlsDepsImportadas, nombreRep)
 
     return urlsDepsImportadas
+
+def __importarDepsPython():
+
+    depsPython = []
+    carpetas = os.listdir()
+    carpetas.remove('Modulador')
+    for carpeta in carpetas:
+        if os.path.isfile(carpeta + '/depsPython.txt'):
+            arch = open(carpeta + '/depsPython.txt', 'r')
+            depsPython += [linea.rstrip('\n') for linea in arch.readlines()]
+            arch.close()
+
+    if depsPython:
+        os.system("py -m venv entVir")
+        install = list(set(['pip install ' + dep for dep in depsPython]))
+        install = ['call entVir\Scripts/activate.bat'] + install + ['deactivate']
+        install = " & ".join(install)
+        os.system(install)
 
 def importarRepositorio(urlRep):
 
@@ -40,21 +58,7 @@ def importarRepositorio(urlRep):
 
         # Importa las dependencias de git
         if os.path.isfile(nombreRep + '/depsGit.txt'):
-            __importarDependencias([], nombreRep)
+            __importarDepsGit([], nombreRep)
 
-        # Importa las dependencias de python
-        os.system("py -m venv entVir")
-
-        depsPython = []
-        carpetas = os.listdir()
-        carpetas.remove('entVir')
-        carpetas.remove('Modulador')
-        for carpeta in carpetas:
-            if os.path.isfile(carpeta + '/depsPython.txt'):
-                arch = open(carpeta + '/depsPython.txt', 'r')
-                depsPython += [linea.rstrip('\n') for linea in arch.readlines()]
-                arch.close()
-        install = list(set(['pip install ' + dep for dep in depsPython]))
-        install = ['call entVir\Scripts/activate.bat'] + install + ['deactivate']
-        install = " & ".join(install)
-        os.system(install)
+        # Importa las dependencias de python en un entorno virtual
+        __importarDepsPython()
