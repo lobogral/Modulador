313f772cefd2d812569afca63758f5ab45f15cb5
Camilo Sabogal <lobogral@gmail.com>
2021-09-30 17:11:51 -0500

    Modifico importador para usarse con pip

--- src/importador.py
+++ src/importador.py
@@ -3,62 +3,44 @@ Realiza importaciones de modulos para desarrollo
 """
 from sys import path
 import os
+import re
 
-def __importarDepsGit(urlsDepsImportadas, nombreRep):
-    arch = open(nombreRep + '/depsGit.txt', 'r')
-    urlsDepsImportar = [lineaArch.rstrip('\n') for lineaArch in arch.readlines()]
+def __importarDepsGit(urlsDepsImportadas, rutArch):
+    arch = open(rutArch, 'r')
+    repositorios = re.findall('https://github.com/\w+/\w+', arch.read())
+    urlsDepsImportar = [lineaArch.rstrip('\n') for lineaArch in repositorios]
     urlsDepsImportar = [val for val in urlsDepsImportar if val not in urlsDepsImportadas]
     arch.close()
     
     for urlDepImportar in urlsDepsImportar:
         if urlDepImportar not in urlsDepsImportadas:
+            nombreRepDep = urlDepImportar.split('/').pop()
+            rutArch = nombreRepDep + '\setup.py'
             os.system('git clone ' + urlDepImportar)
-            nombreRep = urlDepImportar.split('/').pop()
+            os.system('pip install --no-deps -e ' + nombreRepDep)
             urlsDepsImportadas += [urlDepImportar]
-            if os.path.isfile(nombreRep + '/depsGit.txt'):
-                urlsDepsImportadas = __importarDepsGit(urlsDepsImportadas, nombreRep)
+            if os.path.isfile(rutArch):
+                urlsDepsImportadas = __importarDepsGit(urlsDepsImportadas, rutArch)
 
     return urlsDepsImportadas
-
-def __importarDepsPython():
-
-    depsPython = []
-    carpetas = os.listdir()
-    carpetas.remove('Modulador')
-    for carpeta in carpetas:
-        if os.path.isfile(carpeta + '/depsPython.txt'):
-            arch = open(carpeta + '/depsPython.txt', 'r')
-            depsPython += [linea.rstrip('\n') for linea in arch.readlines()]
-            arch.close()
-
-    if depsPython:
-        os.system("py -m venv entVir")
-        install = list(set(['pip install ' + dep for dep in depsPython]))
-        install = ['call entVir\Scripts/activate.bat'] + install + ['deactivate']
-        install = " & ".join(install)
-        os.system(install)
 
 def importarRepositorio(urlRep):
 
     nombreRep = urlRep.split('/').pop()
-    
-    # Agrego en un archivo temporal el nombre del repositorio
-    os.chdir('..')
-    if (not os.path.isdir('temp')): os.mkdir('temp')
-    os.chdir('temp')
-    arch = open("code.txt", "w")
-    arch.write(nombreRep)
-    arch.close()
 
     # Importa el repositorio correspondiente
     os.chdir('../..')
 
     if (not os.path.isdir(nombreRep)):
+
         os.system('git clone ' + urlRep)
 
         # Importa las dependencias de git
+        rutArch = ""    
+        if os.path.isfile(nombreRep + '/setup.py'):
+            rutArch = nombreRep + "/setup.py"
+        if os.path.isfile(nombreRep + '/requirements.txt'):
+            rutArch = nombreRep + "/requirements.txt"
+    
         if os.path.isfile(nombreRep + '/depsGit.txt'):
-            __importarDepsGit([], nombreRep)
-
-        # Importa las dependencias de python en un entorno virtual
-        __importarDepsPython()
+            __importarDepsGit([], rutArch)
